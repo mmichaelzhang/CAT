039f1fbe7add55c06d43bf9653ff52f6bc9e50bb
Michael Zhang <michaelzhangshengming@gmail.com>
2022-03-01 02:44:35 -0500

    Add files via upload

---
+++ main_cat.py
@@ -0,0 +1,83 @@
+import argparse
+import os
+import torch
+
+from exp.exp_cat import Exp_CAT
+
+parser = argparse.ArgumentParser(description='Content-Aware Transformer')
+parser.add_argument('--data', type=str, required=True, default='HDFS', help='data')
+parser.add_argument('--root_path', type=str, default='./data/HDFS/', help='root path of the data file')
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+parser.add_argument('--seq_len', type=int, default=4, help='input sequence length of CAT encoder')
+parser.add_argument('--label_len', type=int, default=4, help='start token length of CAT decoder')
+parser.add_argument('--pred_len', type=int, default=8, help='prediction sequence length')
+# CAT decoder input: concat[start token series(label_len), zero padding series(pred_len)]
+parser.add_argument('--enc_in', type=int, default=1, help='encoder input size')
+parser.add_argument('--dec_in', type=int, default=1, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=1, help='output size')
+parser.add_argument('--d_model', type=int, default=32, help='dimension of model')
+parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+parser.add_argument('--factor', type=int, default=5, help='probsparse attn factor')
+parser.add_argument('--padding', type=int, default=0, help='padding type')
+parser.add_argument('--distil', action='store_false', help='whether to use distilling in encoder, using this argument means not using distilling', default=True)
+parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+parser.add_argument('--attn', type=str, default='prob', help='attention used in encoder, options:[prob, full]')
+parser.add_argument('--embed', type=str, default='timeF', help='time features encoding, options:[timeF, fixed, learned]')
+parser.add_argument('--activation', type=str, default='gelu',help='activation')
+parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+parser.add_argument('--mix', action='store_false', help='use mix attention in generative decoder', default=True)
+parser.add_argument('--cols', type=str, nargs='+', help='certain cols from the data files as the input features')
+parser.add_argument('--num_workers', type=int, default=0, help='data loader num workers')
+parser.add_argument('--itr', type=int, default=1, help='experiments times')
+parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
+parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+parser.add_argument('--des', type=str, default='test',help='exp description')
+parser.add_argument('--loss', type=str, default='mse',help='loss function')
+parser.add_argument('--lradj', type=str, default='type1',help='adjust learning rate')
+parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
+parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+parser.add_argument('--devices', type=str, default='0,1,2,3',help='device ids of multile gpus')
+
+args = parser.parse_args()
+
+args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+
+if args.use_gpu and args.use_multi_gpu:
+    args.devices = args.devices.replace(' ','')
+    device_ids = args.devices.split(',')
+    args.device_ids = [int(id_) for id_ in device_ids]
+    args.gpu = args.device_ids[0]
+
+print('Args in experiment:')
+print(args)
+
+Exp = Exp_CAT
+
+for ii in range(args.itr):
+    # setting record of experiments
+    setting = '{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_at{}_fc{}_eb{}_dt{}_mx{}_{}_{}'.format(args.data, 
+                args.seq_len, args.label_len, args.pred_len,
+                args.d_model, args.n_heads, args.e_layers, args.d_layers, args.d_ff, args.attn, args.factor, 
+                args.embed, args.distil, args.mix, args.des, ii)
+    
+    exp = Exp(args) # set experiments
+    print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+    exp.train(setting)
+    
+    print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+    exp.test(setting)
+
+    if args.do_predict:
+        print('>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+        exp.predict(setting, True)
+
+    torch.cuda.empty_cache()
---
+++ models/model.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from utils.masking import TriangularCausalMask, ProbMask
+from models.encoder import Encoder, EncoderLayer, ConvLayer, EncoderStack
+from models.decoder import Decoder, DecoderLayer
+from models.attn import FullAttention, ProbAttention, AttentionLayer
+from models.embed import DataEmbedding
+
+class CAT(nn.Module):
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
+                factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
+                dropout=0.0, attn='prob', embed='fixed', activation='gelu', 
+                output_attention = False, distil=True, mix=True,
+                device=torch.device('cuda:0'), dec_in_size = 4):
+        super(CAT, self).__init__()
+        self.pred_len = out_len
+        self.attn = attn
+        self.output_attention = output_attention
+
+        # Encoding
+        self.enc_embedding = DataEmbedding(enc_in, d_model, embed, dropout)
+        self.project_input = nn.Linear(dec_in, dec_in_size, bias = True)
+        self.dec_embedding = DataEmbedding(dec_in_size, d_model, embed, dropout)
+        # Attention
+        Attn = ProbAttention if attn=='prob' else FullAttention
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    dropout=dropout,
+                    activation=activation
+                ) for l in range(e_layers)
+            ],
+            [
+                ConvLayer(
+                    d_model
+                ) for l in range(e_layers-1)
+            ] if distil else None,
+            norm_layer=torch.nn.LayerNorm(d_model)
+        )
+        # Decoder
+        self.decoder = Decoder(
+            [
+                DecoderLayer(
+                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=mix),
+                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    dropout=dropout,
+                    activation=activation,
+                )
+                for l in range(d_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(d_model)
+        )
+        self.projection = nn.Linear(d_model, c_out, bias=True)
+        
+        
+    def forward(self, x_enc, x_dec, x_mark_enc = None, x_mark_dec = None, 
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+
+        x_dec = self.project_input(x_dec)
+        dec_out = self.dec_embedding(x_dec, x_mark_dec)
+        dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
+        dec_out = self.projection(dec_out)
+        
+        if self.output_attention:
+            return dec_out[:,-self.pred_len:,:], attns
+        else:
+            return dec_out[:,-self.pred_len:,:] # [B, L, D]
